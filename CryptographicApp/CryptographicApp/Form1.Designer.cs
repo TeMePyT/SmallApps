59b4690a3a90544a278e862266ef8d4d70d3c451
TeMePyT <buhlio@abv.bg>
2018-06-28 16:49:58 +0300

    Initial uploads

---
+++ CryptographicApp/CryptographicApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CryptographicApp
+{
+    partial class CryptoApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CryptoApp));
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonEncryptFile.Location = new System.Drawing.Point(43, 37);
+            this.buttonEncryptFile.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(140, 37);
+            this.buttonEncryptFile.TabIndex = 0;
+            this.buttonEncryptFile.Text = "Encrypt file";
+            this.buttonEncryptFile.UseVisualStyleBackColor = false;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonDecryptFile.Location = new System.Drawing.Point(43, 92);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(140, 37);
+            this.buttonDecryptFile.TabIndex = 1;
+            this.buttonDecryptFile.Text = "Decrypt file";
+            this.buttonDecryptFile.UseVisualStyleBackColor = false;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click_1);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(43, 148);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(140, 37);
+            this.buttonCreateAsmKeys.TabIndex = 2;
+            this.buttonCreateAsmKeys.Text = "Create keys";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = false;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(43, 203);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(140, 37);
+            this.buttonExportPublicKey.TabIndex = 3;
+            this.buttonExportPublicKey.Text = "Export public key";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = false;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(43, 257);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(140, 37);
+            this.buttonImportPublicKey.TabIndex = 4;
+            this.buttonImportPublicKey.Text = "Import public key";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = false;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(43, 313);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(140, 37);
+            this.buttonGetPrivateKey.TabIndex = 5;
+            this.buttonGetPrivateKey.Text = "Get private key";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = false;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files|*.txt";
+            this.openFileDialog1.FilterIndex = 2;
+            this.openFileDialog1.Tag = "";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.Filter = "Encrypted files|*.enc";
+            this.openFileDialog2.FilterIndex = 2;
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CryptoApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(746, 396);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonGetPrivateKey);
+            this.Controls.Add(this.buttonImportPublicKey);
+            this.Controls.Add(this.buttonExportPublicKey);
+            this.Controls.Add(this.buttonCreateAsmKeys);
+            this.Controls.Add(this.buttonDecryptFile);
+            this.Controls.Add(this.buttonEncryptFile);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Algerian", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "CryptoApp";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cryptographic App";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.CryptoApp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CryptographicApp/CryptographicApp/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace CryptographicApp
+{
+    public partial class CryptoApp : Form
+    {
+        CspParameters cspp = new CspParameters();
+        RSACryptoServiceProvider rsa;
+         string EncrFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+@"\";
+         string DecrFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
+        string SrcFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+         string PubKeyFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\rsaPublicKey.txt";
+        const string keyName = "Key01";
+        public CryptoApp()
+        {
+            InitializeComponent();
+        }
+        private void CryptoApp_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonCreateAsmKeys_Click(object sender, EventArgs e)
+        {
+            cspp.KeyContainerName = keyName;
+            rsa = new RSACryptoServiceProvider(cspp);
+            rsa.PersistKeyInCsp = true;
+            if (rsa.PublicOnly == true)
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Public Only";
+            else
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Full Key Pair";
+        }
+        private void buttonEncryptFile_Click(object sender, EventArgs e)
+        {
+            if (rsa == null)
+                MessageBox.Show("Key not set.");
+            else
+            {
+                openFileDialog1.InitialDirectory = SrcFolder;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = openFileDialog1.FileName;
+                    if (fName != null)
+                    {
+                        FileInfo fInfo = new FileInfo(fName);
+                        string name = fInfo.FullName;
+                        EncryptFile(name);
+                    }
+                }
+            }
+        }
+        private void EncryptFile(string inFile)
+        {
+            RijndaelManaged rjndl = new RijndaelManaged();
+            rjndl.KeySize = 256;
+            rjndl.BlockSize = 256;
+            rjndl.Mode = CipherMode.CBC;
+            ICryptoTransform transform = rjndl.CreateEncryptor();
+            byte[] keyEncrypted = rsa.Encrypt(rjndl.Key, false);
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+            int lKey = keyEncrypted.Length;
+            LenK = BitConverter.GetBytes(lKey);
+            int lIV = rjndl.IV.Length;
+            LenIV = BitConverter.GetBytes(lIV);
+            int startFileName = inFile.LastIndexOf("\\") + 1;
+            string outFile = EncrFolder + inFile.Substring(startFileName, inFile.LastIndexOf(".") - startFileName) + ".enc";
+            using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+            {
+                outFs.Write(LenK, 0, 4);
+                outFs.Write(LenIV, 0, 4);
+                outFs.Write(keyEncrypted, 0, lKey);
+                outFs.Write(rjndl.IV, 0, lIV);
+                using (CryptoStream outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                {
+                    int count = 0;
+                    int offset = 0;
+                    int blockSizeBytes = rjndl.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+                    int bytesRead = 0;
+                    using (FileStream inFs = new FileStream(inFile, FileMode.Open))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamEncrypted.Write(data, 0, count);
+                            bytesRead += blockSizeBytes;
+                        }
+                        while (count > 0);
+                        inFs.Close();
+                    }
+                    outStreamEncrypted.FlushFinalBlock();
+                    outStreamEncrypted.Close();
+                }
+                outFs.Close();
+            }
+        }
+        private void buttonExportPublicKey_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(EncrFolder);
+            StreamWriter sw = new StreamWriter(PubKeyFile, false);
+            sw.Write(rsa.ToXmlString(false));
+            sw.Close();
+        }
+        private void buttonImportPublicKey_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(PubKeyFile);
+            cspp.KeyContainerName = keyName;
+            rsa = new RSACryptoServiceProvider(cspp);
+            string keytxt = sr.ReadToEnd();
+            rsa.FromXmlString(keytxt);
+            rsa.PersistKeyInCsp = true;
+            if (rsa.PublicOnly == true)
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Public Only";
+            else
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Full Key Pair";
+            sr.Close();
+        }
+        private void buttonGetPrivateKey_Click(object sender, EventArgs e)
+        {
+            cspp.KeyContainerName = keyName;
+
+            rsa = new RSACryptoServiceProvider(cspp);
+            rsa.PersistKeyInCsp = true;
+
+            if (rsa.PublicOnly == true)
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Public Only";
+            else
+                label1.Text = "Key: " + cspp.KeyContainerName + " - Full Key Pair";
+        }
+
+        private void DecryptFile(string inFile)
+        {
+            RijndaelManaged rjndl = new RijndaelManaged();
+            rjndl.KeySize = 256;
+            rjndl.BlockSize = 256;
+            rjndl.Mode = CipherMode.CBC;
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+            string outFile = DecrFolder + inFile.Substring(0, inFile.LastIndexOf(".")) + ".txt";
+            using (FileStream inFs = new FileStream(EncrFolder + inFile, FileMode.Open))
+            {
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenK, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+                int lenK = BitConverter.ToInt32(LenK, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+                byte[] KeyEncrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Read(KeyEncrypted, 0, lenK);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(IV, 0, lenIV);
+                Directory.CreateDirectory(DecrFolder);
+                byte[] KeyDecrypted = rsa.Decrypt(KeyEncrypted, false);
+                ICryptoTransform transform = rjndl.CreateDecryptor(KeyDecrypted, IV);
+                using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+                {
+                    int count = 0;
+                    int offset = 0;
+                    int blockSizeBytes = rjndl.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+                    inFs.Seek(startC, SeekOrigin.Begin);
+                    using (CryptoStream outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        }
+                        while (count > 0);
+                        outStreamDecrypted.FlushFinalBlock();
+                        outStreamDecrypted.Close();
+                    }
+                    outFs.Close();
+                }
+                inFs.Close();
+            }
+
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDecryptFile_Click_1(object sender, EventArgs e)
+        {
+            if (rsa == null)
+                MessageBox.Show("Key not set.");
+            else
+            {
+                openFileDialog2.InitialDirectory = EncrFolder;
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = openFileDialog2.FileName;
+                    if (fName != null)
+                    {
+                        FileInfo fi = new FileInfo(fName);
+                        string name = fi.Name;
+                        DecryptFile(name);
+                    }
+                }
+            }
+        }
+    }
+}
